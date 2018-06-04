4d57fb56d293deb184bd1600b44dfa36f91d05ae
Hassan Ali <helfi92@gmail.com>
2018-06-04 13:14:53 -0400

    Add resolvers to change fields with underscore

--- src/resolvers/AwsProvisionerWorkerTypes.js
+++ src/resolvers/AwsProvisionerWorkerTypes.js
@@ -1,4 +1,47 @@
 export default {
+  /* eslint-disable camelcase */
+  AwsProvisionerTerminationHealth: {
+    cleanShutdown({ clean_shutdown }) {
+      return clean_shutdown;
+    },
+    spotKill({ spot_kill }) {
+      return spot_kill;
+    },
+    insufficientCapacity({ insufficient_capacity }) {
+      return insufficient_capacity;
+    },
+    volumeLimitExceeded({ volume_limit_exceeded }) {
+      return volume_limit_exceeded;
+    },
+    missingAmi({ missing_ami }) {
+      return missing_ami;
+    },
+    startupFailed({ startup_failed }) {
+      return startup_failed;
+    },
+    unknownCodes({ unknown_codes }) {
+      return unknown_codes;
+    },
+    noCode({ no_code }) {
+      return no_code;
+    },
+  },
+  AwsProvisionerRequestHealth: {
+    configurationIssue({ configuration_issue }) {
+      return configuration_issue;
+    },
+    throttledCalls({ throttled_calls }) {
+      return throttled_calls;
+    },
+    insufficientCapacity({ insufficient_capacity }) {
+      return insufficient_capacity;
+    },
+    limitExceeded({ limit_exceeded }) {
+      return limit_exceeded;
+    },
+  },
+  /* eslint-enable camelcase */
+
   AwsProvisionerWorkerType: {
     state({ workerType }, args, { loaders }) {
       return loaders.awsProvisionerWorkerTypeState.load(workerType);
