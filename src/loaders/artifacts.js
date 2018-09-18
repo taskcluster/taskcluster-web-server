548469c5bfb79b4ade3d8aa9635a72fef60e2b6c
Hassan Ali <helfi92@gmail.com>
2018-09-18 15:18:29 -0400

    Return url field when fetching latest artifacts

--- src/loaders/artifacts.js
+++ src/loaders/artifacts.js
@@ -1,16 +1,29 @@
 import DataLoader from 'dataloader';
 import sift from 'sift';
+import { isNil } from 'ramda';
 import ConnectionLoader from '../ConnectionLoader';
 import Artifact from '../entities/Artifact';
 import Artifacts from '../entities/Artifacts';
 
 export default ({ queue }, isAuthed) => {
-  const withUrl = (taskId, runId, artifact) => {
-    const url = isAuthed
-      ? queue.buildSignedUrl(queue.getArtifact, taskId, runId, artifact.name)
-      : queue.buildUrl(queue.getArtifact, taskId, runId, artifact.name);
+  const withUrl = ({ method, taskId, artifact, runId }) => {
+    const hasRunId = !isNil(runId);
 
-    return { ...artifact, url };
+    if (isAuthed) {
+      return {
+        ...artifact,
+        url: hasRunId
+          ? queue.buildSignedUrl(method, taskId, runId, artifact.name)
+          : queue.buildSignedUrl(method, taskId, artifact.name),
+      };
+    }
+
+    return {
+      ...artifact,
+      url: hasRunId
+        ? queue.buildUrl(method, taskId, runId, artifact.name)
+        : queue.buildUrl(method, taskId, artifact.name),
+    };
   };
 
   const artifact = new DataLoader(queries =>
@@ -18,7 +31,11 @@ export default ({ queue }, isAuthed) => {
       queries.map(async ({ taskId, runId, name }) => {
         const artifact = await queue.getArtifact(taskId, runId, name);
 
-        return new Artifact(taskId, withUrl(taskId, runId, artifact), runId);
+        return new Artifact(
+          taskId,
+          withUrl(queue.getArtifact, taskId, artifact, runId),
+          runId
+        );
       })
     )
   );
@@ -26,7 +43,12 @@ export default ({ queue }, isAuthed) => {
     async ({ taskId, runId, filter, options }) => {
       const raw = await queue.listArtifacts(taskId, runId, options);
       const withUrls = raw.artifacts.map(artifact =>
-        withUrl(taskId, runId, artifact)
+        withUrl({
+          method: queue.getArtifact,
+          taskId,
+          artifact,
+          runId,
+        })
       );
       const artifacts = filter ? sift(filter, withUrls) : withUrls;
 
@@ -36,7 +58,14 @@ export default ({ queue }, isAuthed) => {
   const latestArtifacts = new ConnectionLoader(
     async ({ taskId, filter, options }) => {
       const raw = await queue.listLatestArtifacts(taskId, options);
-      const artifacts = filter ? sift(filter, raw.artifacts) : raw.artifacts;
+      const withUrls = raw.artifacts.map(artifact =>
+        withUrl({
+          method: queue.getLatestArtifact,
+          taskId,
+          artifact,
+        })
+      );
+      const artifacts = filter ? sift(filter, withUrls) : withUrls;
 
       return new Artifacts(taskId, null, { ...raw, artifacts });
     }
