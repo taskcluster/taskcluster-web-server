d3cc0590488cf47801934d535dc879d23120ac6c
Hassan Ali <helfi92@gmail.com>
2018-10-09 15:35:02 -0400

    Remove usage of neutrino-preset-mozilla-frontend-infra

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,26 +1,71 @@
+const { merge } = require('@neutrinojs/compile-loader');
+
 module.exports = {
   use: [
-    ['neutrino-preset-mozilla-frontend-infra/node', {
-      hot: false,
-      babel: {
-        plugins: [
-          require.resolve('@babel/plugin-proposal-object-rest-spread'),
-        ],
-      },
+    ['@neutrinojs/airbnb-base', {
       eslint: {
+        baseConfig: {
+          extends: ['eslint-config-prettier'],
+        },
+        plugins: ['eslint-plugin-prettier'],
         rules: {
           'no-nested-ternary': 0,
+          // Specify the maximum length of a line in your program
+          'max-len': [
+            'error',
+            80,
+            2,
+            {
+              ignoreUrls: true,
+              ignoreComments: false,
+              ignoreStrings: true,
+              ignoreTemplateLiterals: true,
+            },
+          ],
+          // Allow console during development, otherwise throw an error
+          'no-console': process.env.NODE_ENV === 'development' ? 'off' : 'error',
+          // Our frontend strives to adopt functional programming practices,
+          // so we prefer const over let
+          'prefer-const': 'error',
+          'prettier/prettier': [
+            'error',
+            {
+              singleQuote: true,
+              trailingComma: 'es5',
+              bracketSpacing: true,
+              jsxBracketSameLine: true,
+            },
+          ],
+          'class-methods-use-this': 'off',
+          'no-shadow': 'off',
+          'babel/new-cap': 'off',
         },
       },
     }],
-    // replace start-server with restart-server
+    ['@neutrinojs/node', {
+      hot: false,
+    }],
     (neutrino) => {
-      neutrino.config.when(process.env.NODE_ENV === 'development', config => {
-        config.plugins.delete('start-server');
-        neutrino.use(['neutrino-middleware-restart-server', {
-          name: 'index.js',
-        }]);
-      });
+      // Decorators generally need to be enabled *before* other
+      // syntax which exists in both normal plugins, and
+      // development environment plugins.
+      // Tap into the existing Babel options and merge our
+      // decorator options *before* the rest of the existing
+      // Babel options
+      neutrino.config.module
+        .rule('compile')
+        .use('babel')
+        .tap(options =>
+          merge(
+            {
+              plugins: [
+                [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+                require.resolve('@babel/plugin-proposal-class-properties'),
+              ],
+            },
+            options
+          )
+        );
     },
     (neutrino) => {
       neutrino.config.module
@@ -28,12 +73,6 @@ module.exports = {
           .test(/\.graphql$/)
           .use('raw')
             .loader(require.resolve('raw-loader'));
-    },
-    // work around https://bugzilla.mozilla.org/show_bug.cgi?id=1489273
-    (neutrino) => {
-      neutrino.config.when(process.env.NODE_ENV === 'production', config => {
-        config.plugins.delete('named-chunks');
-      });
     },
     '@neutrinojs/mocha'
   ],
