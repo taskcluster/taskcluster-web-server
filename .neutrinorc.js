9d792dca9d38dc00eab5e0fbe24f9aaedece9340
Hassan Ali <helfi92@gmail.com>
2018-10-09 16:06:06 -0400

    Add requested nit

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,5 +1,3 @@
-const { merge } = require('@neutrinojs/compile-loader');
-
 module.exports = {
   use: [
     ['@neutrinojs/airbnb-base', {
@@ -44,29 +42,13 @@ module.exports = {
     }],
     ['@neutrinojs/node', {
       hot: false,
+      babel: {
+        plugins: [
+          [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+          require.resolve('@babel/plugin-proposal-class-properties'),
+        ]
+      },
     }],
-    (neutrino) => {
-      // Decorators generally need to be enabled *before* other
-      // syntax which exists in both normal plugins, and
-      // development environment plugins.
-      // Tap into the existing Babel options and merge our
-      // decorator options *before* the rest of the existing
-      // Babel options
-      neutrino.config.module
-        .rule('compile')
-        .use('babel')
-        .tap(options =>
-          merge(
-            {
-              plugins: [
-                [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-                require.resolve('@babel/plugin-proposal-class-properties'),
-              ],
-            },
-            options
-          )
-        );
-    },
     (neutrino) => {
       neutrino.config.module
         .rule('graphql')
